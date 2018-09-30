24dc8df11def49330bb93ce11cb3a77611be90e8
Lucas Lollobrigida <lucaslollobrigida@gmail.com>
2018-09-30 14:51:32 -0300

    Fix missing configurations on middlewares

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ import dotenv from 'dotenv';
 dotenv.config('.env');
 
 import logger from './utils/logger';
+import { connect } from './utils/database';
 
 import personRouter from './routes/personRouter';
 import discussionRouter from './routes/discussionRouter';
@@ -16,32 +17,39 @@ const port = process.env.PORT || 3000;
 const isDev = process.env.NODE_ENV !== 'production';
 const rootURL = isDev ? `http://localhost` : process.env.URL;
 const mongoURL = isDev ? process.env.MONGO_URL_TEST : process.env.MONGO_URL;
+const mongoDatabase = process.env.MONGO_DB_NAME;
 const sessionSecret = process.env.SESSION_SECRET;
 
-mongoose.connect(mongoURL, { useNewUrlParser: true });
-mongoose.Promise = global.Promise;
+connect(mongoURL, mongoDatabase)
+  .then(db => {
+    const server = express();
 
-mongoose.connection.on('error', err => logger.error(`> ${err}`));
-mongoose.connection.on('connected', () => logger.info(`> Connected to MongoDB instance on ${mongoURL}`));
-mongoose.connection.on('disconnected', () => logger.info(`> Disconnected from MongoDB instance on ${mongoURL}`));
+    server.locals.db = db;
 
-const server = express();
+    server.set('base', rootURL);
+    server.use(helmet());
+    if (isDev) {  //Prevent memory leak, must connect to a store for production(ex. mongo)
+      server.use(session({ 
+        secret: sessionSecret,
+        resave: true,
+        saveUninitialized: true
+      }))
+    }
+    server.use(express.json());
+    server.use(express.urlencoded({ extended: false }));
 
-server.set('base', rootURL);
-server.use(helmet());
-if (isDev) {  //Prevent memory leak, must connect to a store for production(ex. mongo)
-  server.use(session({ secret: sessionSecret }))
-}
-server.use(express.json());
-server.use(express.urlencoded({ extended: false }));
+    server.use('/person', personRouter);
+    server.use('/discussion', discussionRouter);
 
-server.use('/person', personRouter);
-server.use('/discussion', discussionRouter);
-
-server.listen(port, err => {
-  if (err) {
-    logger.error(`> ${err}`);
-    process.exit(1);
-  }
-  logger.info(`> Service running on ${rootURL} port ${port}`);
-});
+    server.listen(port, err => {
+      if (err) {
+        logger.error(`> ${err}`);
+        process.exit(1);
+      }
+      logger.info(`> Service running on ${rootURL} port ${port}`);
+    });
+  })
+  .catch(err => {
+    logger.error(`> Cannot find database. Aborting`);
+    process.end(1);
+  });
